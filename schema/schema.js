5c0294f55c61e1fc72dd8f185f948c03daea32ca
bentonwong <bentonwong@gmail.com>
2017-11-08 11:20:41 -0800

    fix bug with company query

--- schema/schema.js
+++ schema/schema.js
@@ -12,23 +12,23 @@ const {
 
 const CompanyType = new GraphQLObjectType({
   name: 'Company',
-  fields: {
+  fields: () => ({
     id: { type: GraphQLString },
     name: { type: GraphQLString },
     description: { type: GraphQLString },
     users: {
       type: new GraphQLList(UserType),
       resolve(parentValue, args) {
-        return axios.get(`http://localhost:3000/companies/${paretnValue.id}/users`)
+        return axios.get(`http://localhost:3000/companies/${parentValue.id}/users`)
           .then(res => res.data)
       }
     }
-  }
+  })
 });
 
 const UserType = new GraphQLObjectType({
   name: 'User',
-  fields: {
+  fields: () => ({
     id: { type: GraphQLString },
     firstName: { type: GraphQLString },
     age: { type: GraphQLInt },
@@ -36,10 +36,10 @@ const UserType = new GraphQLObjectType({
       type: CompanyType,
       resolve(parentValue, args) {
         return axios.get(`http://localhost:3000/companies/${parentValue.companyId}`)
-          .then(res => res.data)
+          .then(res => res.data);
       }
     }
-  }
+  })
 });
 
 const RootQuery = new GraphQLObjectType({
